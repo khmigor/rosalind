e8ee2338d221ca76446067432057e496ccb47f9b
Igor Mishchenko <migorkh@gmail.com>
2015-07-18 18:44:18 +0300

    [dna] Counting DNA Nucleotides

---
+++ dna.py
@@ -0,0 +1,15 @@
+
+from collections import Counter
+
+
+def main():
+	with open('src')  as src:
+		dna = src.readline()
+		counts = Counter(dna)
+
+		for base in ('A', 'C', 'G', 'T'):
+			print  counts[base], 
+
+
+if __name__ == '__main__':
+	main()
